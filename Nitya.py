8e9829de231fe15ea4dd489245ddee75d2ac8064
Nitya Parmar <nityaparmar1707@gmail.com>
2026-01-17 15:08:43 +0530

    Day 1: The Restart - Jan 17 2026

---
+++ Nitya.py
@@ -0,0 +1,16 @@
+import time
+
+name="Nitya"
+status = "Still Standing"
+dream ="Japan & NextIn"
+
+print(f"Loading {name} v2.0...")
+time.sleep(1)
+
+print("...")
+time.sleep(1)
+
+print(f"Current Status: {status}")
+print(f"Target: {dream}")
+print("\nMESSAGE TO THE WORLD:")
+print("I am not done yet...!!")
